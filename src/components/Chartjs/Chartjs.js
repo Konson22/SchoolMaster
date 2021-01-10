e11a6d994e3c00ddc06e7467fe0aed64f403a09e
KONSONAK <konakech3@gmail.com>
2021-01-10 15:44:31 +0300

    initial commit

--- src/components/Chartjs/Chartjs.js
+++ src/components/Chartjs/Chartjs.js
@@ -4,12 +4,22 @@ import { Bar, Line } from 'react-chartjs-2'
 
 export default function Chartjs() {
     const [data, serData] = useState({
-            labels:['primary one','primary two', 'primary three', 'primary four', 'primary five','primary six','primary seven', 'primary eight'],
+            labels:['primary one','primary two', 'primary three', 'primary four', 'primary five','primary six','primary seven', 'primary eight', 'all classes'],
             datasets:[{
                 label:'paid',
-                data: [560,554,555,548,566,544,554,556],
-                borderColor:'#000',
-                borderColor:'rgba(250,209,15)',
+                data: [80,90,77,90,78,86,80,88, 87],
+                backgroundColor:[
+                    'rgba(50,209,105)',
+                    'rgba(250,29,89)',
+                    'rgba(10,69,5)',
+                    'rgba(25,249,32)',
+                    'rgba(250,29,9)',
+                    'rgba(260,79,95)',
+                    'rgba(105,44,28)',
+                    'rgba(210,9,9)',
+                    'rgba(260,179,95)'
+                ],
+                borderColor:'rgba(250,209,105)',
                 borderWidth:1,
                 lineTension:0.1
             }]
@@ -17,7 +27,7 @@ export default function Chartjs() {
 
     return (
         <div>
-            <Line 
+            <Bar
                 data={data}
             />
         </div>
